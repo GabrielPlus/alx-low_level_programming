3ced7077f2e088a797d8e5413263b7d54cfef0b0
gabby2001 <gabrielplus2001@gmail.com>
2022-06-02 10:40:18 +0000

    output without print f

---
+++ 4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * prints a string to standard output
+ * Return:0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

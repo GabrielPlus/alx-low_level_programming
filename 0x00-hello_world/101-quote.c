5449a5f3a9738ee5e32e89e34b326cc817610cbf
gabby2001 <105011841+gabby2001@users.noreply.github.com>
2022-06-29 15:02:47 +0300

    added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
+
  * Return: Always  0 (success)
  */
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19"\n,
 	sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
 return (1);
-=======
+}
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -19,5 +19,5 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
->>>>>>> 1aed404ab7bb442f521184d4ef6c7e3a2a474690
+
 }
